9049598272e35e226045135707105d25cd2f4ce5
YounesAEO <y.aboueleomoum09@gmail.com>
2022-08-22 16:47:54 +0200

    added logEntry schema using mongoose

---
+++ server/models/LogEntry.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const reqString = {
+	type: String,
+	required: true,
+};
+const reqNum = {
+	type: Number,
+	required: true,
+};
+
+const logEntrySchema = new Schema(
+	{
+		title: reqString, // String is shorthand for {type: String}
+		description: String,
+		rating: { type: Number, min: 1, max: 5, default: 1 },
+		comments: [{ body: String, date: Date }],
+		latitude: { ...reqNum, min: -90, max: 90 },
+		longitude: { ...reqNum, min: -180, max: 180 },
+		visitDate: {
+			type: Date,
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const LogEntry = mongoose.model('LogEntry', logEntrySchema);
+
+module.exports = LogEntry;
