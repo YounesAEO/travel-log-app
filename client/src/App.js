86dca46e7be1d2e4a4eefd8f03a3b1fbd974f14e
YounesAEO <y.aboueleomoum09@gmail.com>
2022-08-28 14:37:48 +0200

    added basic ui to show travel log details

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Map, { Marker } from 'react-map-gl';
+import Map, { Marker, Popup } from 'react-map-gl';
 import { fetchLogEntries } from './api';
 
 const { useEffect, useState } = React;
@@ -11,6 +11,8 @@ function App() {
 		latitude: 40,
 		zoom: 3.5,
 	});
+	const [showPopup, setShowPopup] = useState({});
+
 	useEffect(() => {
 		(async () => {
 			const entries = await fetchLogEntries();
@@ -25,29 +27,58 @@ function App() {
 			style={{ width: '100vw', height: '100vh' }}
 			mapStyle="mapbox://styles/younesaeo/cl7byjp7k002q14mr8ovau2zo"
 			mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}>
-			{entries.map((entry) => {
+			{entries.map((entry, index) => {
 				return (
-					<Marker
-						key={entry._id}
-						longitude={entry.longitude}
-						latitude={entry.latitude}
-						anchor="bottom">
-						<svg
-							viewBox="0 0 24 24"
-							style={{
-								width: '24px',
-								height: '24px',
-								stroke: '#96f97b',
-							}}
-							stroke="currentColor"
-							strokeWidth="2"
-							fill="none"
-							strokeLinecap="round"
-							strokeLinejoin="round">
-							<path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
-							<circle cx="12" cy="10" r="3"></circle>
-						</svg>
-					</Marker>
+					<>
+						<Marker
+							key={`marker${index}`}
+							longitude={entry?.longitude}
+							latitude={entry?.latitude}
+							anchor="bottom">
+							<div
+								onClick={() =>
+									setShowPopup({
+										[entry?._id?.toString()]: true,
+									})
+								}>
+								<svg
+									viewBox="0 0 24 24"
+									style={{
+										width: '24px',
+										height: '24px',
+										stroke: '#96f97b',
+									}}
+									stroke="currentColor"
+									strokeWidth="2"
+									fill="none"
+									strokeLinecap="round"
+									strokeLinejoin="round">
+									<path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+									<circle cx="12" cy="10" r="3"></circle>
+								</svg>
+							</div>
+						</Marker>
+						{showPopup[entry?._id?.toString()] ? (
+							<Popup
+								key={`popup${index}`}
+								longitude={entry?.longitude}
+								latitude={entry?.latitude}
+								closeButton={true}
+								closeOnClick={false}
+								onClose={() => setShowPopup({})}>
+								<div className="popup">
+									<h3>{entry?.title}</h3>
+									<p>{entry?.description}</p>
+									<small>
+										Visited on:{' '}
+										{new Date(
+											entry?.visitDate
+										).toLocaleDateString()}
+									</small>
+								</div>
+							</Popup>
+						) : null}
+					</>
 				);
 			})}
 		</Map>
